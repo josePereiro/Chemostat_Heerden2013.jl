3575efe6c9797edce836ea735cb34bde222bc528
josePereiro <jose.pereiro.lab@gmail.com>
2020-12-05 16:07:00 -0500

    fix bug

--- src/HeerdenData/data_interface.jl
+++ src/HeerdenData/data_interface.jl
@@ -2,25 +2,25 @@
 # taken from Heerden2013 https://doi.org/10.1186/1475-2859-12-80.
 
 ## ------------------------------------------------------------------
-# Load data
 cont_cul_data = DataFrame()
+msd_mets = ["GLC", "SA", "AcA", "FA", "MA"]
 
+# Interface
+data_ids()::Vector{String} = string.(names(cont_cul_data))
+val(id)::Vector{Float64} = parse.(Float64, string.(cont_cul_data[3:end, Symbol(id)]))
+val(id, indx::Integer)::Float64 = val(id)[indx]
+val(id, indx::Integer, defval)::Float64 = try; return val(id, indx) catch; return defval end
+val(id, indx)::Vector{Float64} = val(id)[indx]
+name(id)::String = string(cont_cul_data[1, Symbol(id)])
+unit(id)::String = string(cont_cul_data[2, Symbol(id)])
+
+# Load data
 function _load_cont_cul_data()
 
     !isfile(HEERDEN_CONT_CUL_DATA_CONV_FILE) && return cont_cul_data
     
     ## ------------------------------------------------------------------
     global cont_cul_data = CSV.read(HEERDEN_CONT_CUL_DATA_CONV_FILE, DataFrame; delim = '\t', comment = "#")
-
-    # Interface
-    msd_mets = ["GLC", "SA", "AcA", "FA", "MA"]
-    data_ids()::Vector{String} = string.(names(cont_cul_data))
-    val(id)::Vector{Float64} = parse.(Float64, string.(cont_cul_data[3:end, Symbol(id)]))
-    val(id, indx::Integer)::Float64 = val(id)[indx]
-    val(id, indx::Integer, defval)::Float64 = try; return val(id, indx) catch; return defval end
-    val(id, indx)::Vector{Float64} = val(id)[indx]
-    name(id)::String = string(cont_cul_data[1, Symbol(id)])
-    unit(id)::String = string(cont_cul_data[2, Symbol(id)])
 
     ## ------------------------------------------------------------------
     # include xi
