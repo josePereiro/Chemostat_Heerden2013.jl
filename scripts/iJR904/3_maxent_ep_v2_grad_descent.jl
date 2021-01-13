e83950f3058ce3381d6ade201e8197b1b65b40a2
josePereiro <jose.pereiro.lab@gmail.com>
2021-01-12 23:44:33 -0500

    WIP, add fva_preprocess models

--- scripts/iJR904/3_maxent_ep_v2_grad_descent.jl
+++ scripts/iJR904/3_maxent_ep_v2_grad_descent.jl
@@ -1,70 +1,94 @@
 import DrWatson: quickactivate
 quickactivate(@__DIR__, "Chemostat_Heerden2013")
 
-import SparseArrays
-import Base.Threads: @threads, threadid, SpinLock
+@time begin
+    import SparseArrays
+    import Base.Threads: @threads, threadid
+
+    ## -------------------------------------------------------------------
+    # Run add https://github.com/josePereiro/Chemostat_Heerden2013.jl in the Julia Pkg REPL to install the
+    # package, then you must activate the package enviroment (see README)
+    import Chemostat_Heerden2013
+    const ChHd = Chemostat_Heerden2013
+    const Hd  = ChHd.HeerdenData;
+    const BD  = ChHd.BegData;
+    const iJR = ChHd.iJR904
+
+    ## -------------------------------------------------------------------
+    # run add "https://github.com/josePereiro/Chemostat" in the 
+    # julia Pkg REPL for installing the package
+    import Chemostat
+    import Chemostat.LP.MathProgBase
+    const Ch = Chemostat
+    const ChU = Ch.Utils
+    const ChSS = Ch.SteadyState
+    const ChLP = Ch.LP
+    const ChEP = Ch.MaxEntEP
+    const ChSU = Ch.SimulationUtils
+end
 
 ## -------------------------------------------------------------------
-# Run add https://github.com/josePereiro/Chemostat_Heerden2013.jl in the Julia Pkg REPL to install the
-# package, then you must activate the package enviroment (see README)
-import Chemostat_Heerden2013
-const ChHd = Chemostat_Heerden2013
-const Hd  = ChHd.HeerdenData;
-const BD  = ChHd.BegData;
-const iJR = ChHd.iJR904
+# globals
+const WLOCK = ReentrantLock()
+#TODO make a proper caching system
 
 ## -------------------------------------------------------------------
-# run add "https://github.com/josePereiro/Chemostat" in the 
-# julia Pkg REPL for installing the package
-import Chemostat
-import Chemostat.LP.MathProgBase
-const Ch = Chemostat
-const ChU = Ch.Utils
-const ChSS = Ch.SteadyState
-const ChLP = Ch.LP
-const ChEP = Ch.MaxEntEP
-const ChSU = Ch.SimulationUtils
-ChU.set_cache_dir(iJR.CACHE_DIR)
-
-## -------------------------------------------------------------------
-# Simulation
-const DATA = Dict()
+# Prepare fva models
+const DAT_FILE = iJR.MAXENT_FBA_EB_BOUNDLES_FILE
+const DATA = isfile(DAT_FILE) ? ChU.load_data(DAT_FILE) : Dict()
 let
-    verbose_lock = ReentrantLock()
-    FREE = -1
-    verbose_token = FREE
-    
-    cGLCs = Hd.val("cGLC")
-    @threads for (exp, cGLC) in cGLCs |> enumerate |> collect
+    cGLCs = Hd.val("cGLC") |> enumerate |> collect
+    for (exp, cGLC) in cGLCs 
+        
+        @info "Doing" exp cGLC length(DATA) threadid()
+        println()
 
-        # handle cache
-        haskey(DATA, exp) && continue
-
-        # handle verbose
-        imverbose = false
-        lock(verbose_lock) do
-            if verbose_token == FREE
-                verbose_token = threadid()
-                imverbose = true
-            end
-        end
-
-        # model
-        model = ChU.MetNet(ChU.load_data(iJR.BASE_MODEL_FILE; verbose = imverbose))
-        model = ChU.uncompressed_model(model)
-        objidx = ChU.rxnindex(model, iJR.BIOMASS_IDER)
-        M, N = size(model)
-            
-        imverbose && @info "Doing" threadid() exp cGLC length(DATA)
         D = get!(DATA, exp, Dict())
+        haskey(D, "model") && continue # check caching
 
         # prepare model
+        model = ChU.MetNet(ChU.load_data(iJR.BASE_MODEL_FILE; verbose = true))
+        model = ChU.uncompressed_model(model)
+        objidx = ChU.rxnindex(model, iJR.BIOMASS_IDER)
+
         exp_growth = Hd.val("D", exp)
         expξ = Hd.val("xi", exp)
         intake_info = deepcopy(iJR.base_intake_info)
         intake_info[iJR.EX_GLC_IDER]["c"] = cGLC
         ChSS.apply_bound!(model, expξ, intake_info)
-        model = ChLP.fva_preprocess(model; check_obj = iJR.BIOMASS_IDER)
+        model = ChLP.fva_preprocess(model; check_obj = iJR.BIOMASS_IDER, 
+            verbose = true)
+        
+        D["model"] = ChU.compressed_model(model)
+        
+        # caching
+        ChU.save_data(DAT_FILE, DATA; verbose = false)
+    end
+end
+
+## -------------------------------------------------------------------
+# Simulation
+let
+    
+    cGLCs = Hd.val("cGLC") |> enumerate |> collect
+    @threads for (exp, cGLC) in cGLCs 
+
+        # handle cache
+        D = DATA[exp]
+        haskey(D, "epouts") && continue
+
+        # setup
+        model = D["model"]
+        model = ChU.uncompressed_model(model)
+        objidx = ChU.rxnindex(model, iJR.BIOMASS_IDER)
+        M, N = size(model)
+        exp_growth = Hd.val("D", exp)
+        expξ = Hd.val("xi", exp)
+        
+        lock(WLOCK) do
+            @info "Doing" exp cGLC threadid()
+            println()
+        end
         
         # gradien descent
         epouts = Dict()
@@ -73,11 +97,15 @@ let
         C = [5e3]
         th = 1e-3
         epout_seed = nothing
-        target = [Hd.val("D", exp)]
+        target = [exp_growth]
         beta_vec = zeros(size(model, 2))
+
+        upfrec = 10
+        it = 1
 
         function upfun(betas)
             beta = first(betas)
+
             if haskey(epouts, beta) 
                 epout = epouts[beta]
             else
@@ -87,19 +115,28 @@ let
                     alpha = Inf,
                     damp = 0.985,
                     epsconv = 1e-5, 
-                    verbose = imverbose && isempty(epouts), 
+                    verbose = false, 
                     solution = epout_seed,
                     maxiter = 5000
                 )
                 epouts[beta] = epout
             end
             epout_seed = epout
-            r = [ChU.av(epout)[objidx]]
+            curr = [ChU.av(epout)[objidx]]
+
+            if it == 1 || rem(it, upfrec) == 0
+                lock(WLOCK) do
+                    @info "Grad descent... " exp it beta abs.(target .- curr) threadid()
+                    println()
+                    it += 1
+                end
+            end
+            curr
         end
 
         # maxent
         expβ = ChSU.grad_desc(upfun; x0, x1, th, C, 
-            target, maxiters = 1500, verbose = imverbose)
+            target, maxiters = 1500, verbose = false) |> first
 
         # fba
         fbaout = let
@@ -108,47 +145,49 @@ let
             ChLP.fba(lmodel, iJR.BIOMASS_IDER, iJR.COST_IDER)
         end
 
-        # Storing
-        D["model"] = ChU.compressed_model(model)
-        D["exp_beta"] = first(expβ)
-        D["exp_xi"] = expξ
-        D["epouts"] = epouts
-        D["fbaout"] = fbaout
-        D["epouts"] = epouts
-        
-        # release token
-        lock(verbose_lock) do
-            imverbose && (verbose_token = FREE; println())
+        lock(WLOCK) do
+            # Storing
+            D["exp_beta"] = expβ
+            D["exp_xi"] = expξ
+            D["epouts"] = epouts
+            D["fbaout"] = fbaout
+            D["epouts"] = epouts
+
+            # caching
+            ChU.save_data(DAT_FILE, DATA; verbose = false)
+
+            @info "Finished " exp expβ length(epouts) threadid()
+            println()
         end
     end
 end
 
 ## -------------------------------------------------------------------
 # Fva
-let
-    ϵ = 0.95 # relax factor
-    @info "Doing FVA" ϵ
-    for (exp, D) in DATA
+# let
+#     ϵ = 0.95 # relax factor
+#     @info "Doing FVA" ϵ
+#     for (exp, D) in DATA
         
-        model = D["model"]
-        fbaout = D["fbaout"]
+#         model = D["model"]
+#         fbaout = D["fbaout"]
 
-        biom_val = ChU.av(model, fbaout, iJR.BIOMASS_IDER)
-        cost_val = ChU.av(model, fbaout, iJR.COST_IDER)
-        biom_bounds = ChU.bounds(model, iJR.BIOMASS_IDER)
-        cost_bounds = ChU.bounds(model, iJR.COST_IDER)
+#         biom_val = ChU.av(model, fbaout, iJR.BIOMASS_IDER)
+#         cost_val = ChU.av(model, fbaout, iJR.COST_IDER)
+#         biom_bounds = ChU.bounds(model, iJR.BIOMASS_IDER)
+#         cost_bounds = ChU.bounds(model, iJR.COST_IDER)
         
-        @info "Doing" exp biom_bounds cost_bounds
-        ChU.bounds!(model, iJR.BIOMASS_IDER, ϵ * biom_val, biom_val)
-        ChU.bounds!(model, iJR.COST_IDER, ϵ * cost_val, cost_val)
+#         @info "Doing" exp biom_bounds cost_bounds
+#         ChU.bounds!(model, iJR.BIOMASS_IDER, ϵ * biom_val, biom_val)
+#         ChU.bounds!(model, iJR.COST_IDER, ϵ * cost_val, cost_val)
         
-        D["fva"] = ChLP.fva(model)
+#         D["fva"] = ChLP.fva(model)
         
-        ChU.bounds!(model, iJR.BIOMASS_IDER, biom_bounds...)
-        ChU.bounds!(model, iJR.COST_IDER, cost_bounds...)
-    end
-end
+#         ChU.bounds!(model, iJR.BIOMASS_IDER, biom_bounds...)
+#         ChU.bounds!(model, iJR.COST_IDER, cost_bounds...)
+#     end
+# end
 
 ## -------------------------------------------------------------------
 # save results
-ChU.save_data(iJR.MAXENT_FBA_EB_BOUNDLES_FILE, DATA)
+ChU.save_data(DAT_FILE, DATA; verbose = false)
--- scripts/iJR904/5_fixxed_maxent.jl
+++ scripts/iJR904/5_fixxed_maxent.jl
@@ -1,30 +1,37 @@
 import DrWatson: quickactivate
 quickactivate(@__DIR__, "Chemostat_Heerden2013")
 
-import SparseArrays
-import Base.Threads: @threads, threadid, SpinLock
+@time begin
+    import SparseArrays
+    import Base.Threads: @threads, threadid, SpinLock
 
-#  ----------------------------------------------------------------------------
-# Run add https://github.com/josePereiro/Chemostat_Heerden2013.jl in the Julia Pkg REPL to install the
-# package, then you must activate the package enviroment (see README)
-import Chemostat_Heerden2013
-const ChHd = Chemostat_Heerden2013
-const Hd  = ChHd.HeerdenData;
-const BD  = ChHd.BegData;
-const iJR = ChHd.iJR904
+    #  ----------------------------------------------------------------------------
+    # Run add https://github.com/josePereiro/Chemostat_Heerden2013.jl in the Julia Pkg REPL to install the
+    # package, then you must activate the package enviroment (see README)
+    import Chemostat_Heerden2013
+    const ChHd = Chemostat_Heerden2013
+    const Hd  = ChHd.HeerdenData;
+    const BD  = ChHd.BegData;
+    const iJR = ChHd.iJR904
 
-#  ----------------------------------------------------------------------------
-# run add "https://github.com/josePereiro/Chemostat" in the 
-# julia Pkg REPL for installing the package
-import Chemostat
-import Chemostat.LP.MathProgBase
-const Ch = Chemostat
-const ChU = Ch.Utils
-const ChSS = Ch.SteadyState
-const ChLP = Ch.LP
-const ChEP = Ch.MaxEntEP
-const ChSU = Ch.SimulationUtils
-ChU.set_cache_dir(iJR.CACHE_DIR)
+    #  ----------------------------------------------------------------------------
+    # run add "https://github.com/josePereiro/Chemostat" in the 
+    # julia Pkg REPL for installing the package
+    import Chemostat
+    import Chemostat.LP.MathProgBase
+    const Ch = Chemostat
+    const ChU = Ch.Utils
+    const ChSS = Ch.SteadyState
+    const ChLP = Ch.LP
+    const ChEP = Ch.MaxEntEP
+    const ChSU = Ch.SimulationUtils
+end
+
+## ----------------------------------------------------------------------------
+DAT_FILE = "/Users/Pereiro/University/Research/Metabolism/MaxEntEP2020/WIP/"*
+            "Chemostat_Heerden2013/data/processed/iJR904/maxent_fba_ep_bundles copy.bson"
+# DATA = ChU.load_data(DAT_FILE; verbose = false)
+filesize(DAT_FILE)
 
 ## ----------------------------------------------------------------------------
 let
